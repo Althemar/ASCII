a955bedd8208f698128db30906c18df4534a7c72
CRECHET Damien <Damien.crechet@formation.enjmin.fr>
2017-10-02 16:01:25 +0200

    Merged

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -1,15 +1,29 @@
 #include "Screen.h"
 
-Screen::Screen(int width, int height) : _width(width), _height(height)
+Screen::Screen(short width, short height) :
+	_width{ width },
+	_height{ height },
+	_hOutput{ (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE) },
+	_dwBufferSize{ _width,_height },
+	_dwBufferCoord{ 0, 0 },
+	_rcRegion{ 0, 0, _width - 1, _height - 1 },
+	_buffer{ new CHAR_INFO[_width * _height] }
 {
-	_buffer = new CHAR_INFO * [width];
-	for( unsigned i = 0; i < width; ++i )
-		_buffer[i] = new CHAR_INFO[height];
+	ReadConsoleOutput(_hOutput, _buffer, _dwBufferSize,_dwBufferCoord, &_rcRegion);
 }
 
 Screen::~Screen()
 {
-	for (unsigned i = 0; i < _width; ++i)
-		delete[](_buffer[i]);
 	delete[](_buffer);
+}
+
+void Screen::display()
+{
+	WriteConsoleOutput(_hOutput, _buffer, _dwBufferSize,_dwBufferCoord, &_rcRegion);
+}
+
+void Screen::draw( short x, short y, char character, int color)
+{
+	_buffer[_height * x + y].Char.AsciiChar = character;
+	_buffer[_height * x + y].Attributes = color;
 }
